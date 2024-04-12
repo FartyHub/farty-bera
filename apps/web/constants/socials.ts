5e7e536fd5711fedb59c461d6c2589d47ff331ef
e6654321 <enespra@gmail.com>
2024-04-12 21:56:50 +1000

    [Misc][Ram] Added telegram and x socials

--- apps/web/constants/socials.ts
+++ apps/web/constants/socials.ts
@@ -1,3 +1,3 @@
-export const X_URL = 'https://twitter.com/';
-export const TELEGRAM_URL = 'https://t.me/';
+export const X_URL = 'https://twitter.com/fartybera';
+export const TELEGRAM_URL = 'https://t.me/fartybera';
 export const DISCORD_URL = 'https://discord.gg/';
