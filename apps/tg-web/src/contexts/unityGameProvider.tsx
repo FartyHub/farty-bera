35dc8ceb961ffc58b135aa9c04aecc7bc2cabfd6
e6654321 <enespra@gmail.com>
2024-05-30 02:20:39 +0800

    [Misc] Added cors and changed to test builds

--- apps/tg-web/src/contexts/unityGameProvider.tsx
+++ apps/tg-web/src/contexts/unityGameProvider.tsx
@@ -31,17 +31,17 @@ export function UnityGameProvider({ children }: { children: ReactNode }) {
     sendMessage,
     unityProvider,
   } = useUnityContext({
-    codeUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.wasm',
-    dataUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.data',
-    frameworkUrl:
-      'https://storage.googleapis.com/tg-mini-app-build/tg.framework.js',
-    loaderUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.loader.js',
-    // codeUrl: 'https://storage.googleapis.com/tg-mini-app-build/Build.wasm',
-    // dataUrl: 'https://storage.googleapis.com/tg-mini-app-build/Build.data',
+    // codeUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.wasm',
+    // dataUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.data',
     // frameworkUrl:
-    //   'https://storage.googleapis.com/tg-mini-app-build/Build.framework.js',
-    // loaderUrl:
-    //   'https://storage.googleapis.com/tg-mini-app-build/Build.loader.js',
+    //   'https://storage.googleapis.com/tg-mini-app-build/tg.framework.js',
+    // loaderUrl: 'https://storage.googleapis.com/tg-mini-app-build/tg.loader.js',
+    codeUrl: 'https://storage.googleapis.com/tg-mini-app-build/Build.wasm',
+    dataUrl: 'https://storage.googleapis.com/tg-mini-app-build/Build.data',
+    frameworkUrl:
+      'https://storage.googleapis.com/tg-mini-app-build/Build.framework.js',
+    loaderUrl:
+      'https://storage.googleapis.com/tg-mini-app-build/Build.loader.js',
   });
 
   const values = useMemo(
