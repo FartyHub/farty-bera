149f9f0ee320b540a55a8add4ce00e186ca4f7e0
e6654321 <enespra@gmail.com>
2024-08-07 19:17:54 +0800

    [Misc] Update to free ip api

--- apps/tg-web/src/hooks/useIPFuctions.ts
+++ apps/tg-web/src/hooks/useIPFuctions.ts
@@ -1,33 +1,38 @@
-import IPinfoWrapper, { IPinfo } from 'node-ipinfo';
+/* eslint-disable no-console */
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const ipInfoWrapper = new IPinfoWrapper(
-  import.meta.env.VITE_IP_INFO_TOKEN ?? '',
-);
-
 export function useIPFunctions() {
-  const [ip, setIp] = useState<string | undefined>();
-  const [ipInfo, setIpInfo] = useState<IPinfo | undefined>();
+  const [ip, setIp] = useState<string>();
+  const [region, setRegion] = useState<string>();
 
   useEffect(() => {
     (async () => {
       if (ip) {
-        const response = await ipInfoWrapper.lookupIp(ip);
-
-        setIpInfo(response);
+        try {
+          const response = await fetch(`https://freeipapi.com/api/json/${ip}`);
+          const data = await response.json();
+          setRegion(data.continentCode);
+        } catch (error) {
+          console.log('error', error);
+        }
       }
     })();
   }, [ip]);
 
   useEffect(() => {
     (async () => {
-      const response = await fetch('https://ifconfig.me/ip');
-      setIp(await response.text());
+      try {
+        const response = await axios.get('https://ifconfig.me/ip');
+        setIp(response.data);
+      } catch (error) {
+        console.log('error', error);
+      }
     })();
   }, []);
 
   return {
     ip,
-    ipInfo,
+    region,
   };
 }
